927d5284b6f072f5ca5554fb5d06297e3b1fd444
nathaliadutra <nathaliad.oliveira@hotmail.com>
2021-12-16 17:49:01 -0300

    Adicionado requisicoes PUT e DELETE

--- src/controllers/MedController.js
+++ src/controllers/MedController.js
@@ -1,38 +1,62 @@
 const database = require('../database/connection')
 
 class MedController {
-    novoMedicamento(request, response){
-        const {nomeMedicamento, intervaloH, qntdPilula, liquido} = request.body
+    novoMedicamento(request, response) {
+        const { nomeMedicamento, intervaloH, qntdPilula, liquido } = request.body
 
         console.log(nomeMedicamento, intervaloH, qntdPilula, liquido)
 
-        database.insert({nomeMedicamento, intervaloH, qntdPilula, liquido}).table("medicamentos").then(data =>{
+        database.insert({ nomeMedicamento, intervaloH, qntdPilula, liquido }).table("medicamentos").then(data => {
             console.log(data)
-            response.json({message:"Medicamento cadastrado com sucesso!"})
-        }).catch(error =>{
+            response.json({ message: "Medicamento cadastrado com sucesso!" })
+        }).catch(error => {
             console.log(error)
         })
     }
 
-    listarMedicamentos(request, response){
-        database.select("*").table("medicamentos").then(medicamentos =>{
+    listarMedicamentos(request, response) {
+        database.select("*").table("medicamentos").then(medicamentos => {
             console.log(medicamentos)
             response.json(medicamentos)
-        }).catch(error =>{
+        }).catch(error => {
             console.log(error)
         })
     }
 
-    buscaMedicamento(request, response){
+    buscaMedicamento(request, response) {
         const id = request.params.id
 
-        database.select("*").table("medicamentos").where({id:id}).then(medicamento =>{
+        database.select("*").table("medicamentos").where({ id: id }).then(medicamento => {
             response.json(medicamento)
 
         }).catch(error => {
             console.log(error)
         })
     }
+
+    removerMedicamento(request, response) {
+        const id = request.params.id
+
+        database.where({ id: id }).del().table("medicamentos").then(data => {
+            response.json({ message: "Medicamento removido com sucesso" })
+
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    atualizarMedicamento(request, response) {
+        const id = request.params.id
+        const {nomeMedicamento, intervaloH, qntdPilula, liquido} = request.body
+
+        database.where({id:id}).update({nomeMedicamento:nomeMedicamento, intervaloH:intervaloH, qntdPilula:qntdPilula,liquido:liquido}).table("medicamentos").then(data => {
+            response.json({ message: "Medicamento atualizado com sucesso" })
+        }).catch(error => {
+            response.json(error)
+        })
+    }
+
+
 }
 
 module.exports = new MedController();
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,5 +6,8 @@ const MedController = require('../controllers/MedController')
 router.post('/novoMedicamento', MedController.novoMedicamento)
 router.get('/medicamentos', MedController.listarMedicamentos)
 router.get('/medicamentos/:id', MedController.buscaMedicamento)
+router.put('/atualizarMedicamento/:id', MedController.atualizarMedicamento)
+router.delete('/removerMedicamento/:id', MedController.removerMedicamento)
 
 module.exports = router
+
